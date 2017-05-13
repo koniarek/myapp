9de27778fea7f2698ffa98aafa4b4405f60d9827
Paweł <pawel.jakimowicz10@gmail.com>
2017-05-13 08:53:29 +0200

    Add sidebar content menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { Component, ViewChild } from '@angular/core';
+import {Platform, NavController, MenuController } from 'ionic-angular';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -9,14 +7,13 @@ import { HomePage } from '../pages/home/home';
 })
 export class MyApp {
   rootPage:any = HomePage;
+  @ViewChild('nav') nav: NavController
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
+  constructor(platform: Platform, private menuCtrl: MenuController) {};
+
+  onLoad(page: any) {
+    this.nav.setRoot(page);
+    this.menuCtrl.close();
   }
 }
 
