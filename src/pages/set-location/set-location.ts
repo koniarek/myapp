337a96ca8ae5c54a98dd5c8cbbc60dca067bfab9
Paweł <pawel.jakimowicz10@gmail.com>
2017-05-16 22:47:00 +0200

    add confirm location and abort map function

--- src/pages/set-location/set-location.ts
+++ src/pages/set-location/set-location.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ViewController, NavController, NavParams } from 'ionic-angular';
 import { Location } from '../../models/location';
 
-@IonicPage()
 @Component({
   selector: 'page-set-location',
   templateUrl: 'set-location.html',
@@ -13,7 +12,8 @@ export class SetLocationPage {
 
   constructor(
       public navCtrl: NavController,
-      public navParams: NavParams) {
+      public navParams: NavParams,
+      public viewCtrl: ViewController) {
 
       this.location = this.navParams.get('location');
   }
@@ -21,5 +21,11 @@ export class SetLocationPage {
   setMarker(event: any) {
     console.log(event);
     this.marker = new Location(event.coords.lat, event.coords.lng);
+  }
+  confirmLocation() {
+    this.viewCtrl.dismiss({location: this.marker});
+  }
+  abort() {
+    this.viewCtrl.dismiss();
   }
 }
